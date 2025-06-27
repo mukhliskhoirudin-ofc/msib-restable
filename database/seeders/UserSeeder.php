9368c4163a473266333c5ed1e37bf7b8ea6958a7
mukhliskhoirudin-ofc <mukhliskhoirudin.official@gmail.com>
2025-06-27 23:23:43 +0700

    chore: setup template, laravel ui, and user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Operator',
+            'email' => 'operator@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'role' => 'operator'
+        ]);
+
+        User::create([
+            'name' => 'Owner',
+            'email' => 'owner@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'role' => 'owner'
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,24 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Route::prefix('panel')->group(function () {
+//     Route::get('dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+Route::prefix('panel')->middleware(['auth'])->group(function () {
+    Route::get('dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
